c6040bd31958b731f97669fc6ac1d497e03e7689
thetravisweber <tbroweber@gmail.com>
2021-04-24 19:50:49 -0700

    added skills file

---
+++ src/views/Skills.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h2>Skills</h2>
+
+    <div class="skills-container center">
+
+
+    </div>
+
+    
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: "Skills",
+};
+</script>
+
+<style>
+
+.skills-container {
+  width: 60%;
+  min-height: 100%;
+  border-width: 0 8px 0 8px; /* top right bottom left */
+  border-style: solid double;
+  border-color: var(--light-secondary);
+}
+
+.skills-description {
+  text-align: left;
+  text-indent: 25px;
+  color: var(--black);
+}
+
+</style>
