46aafb772bca0a1f30ad8f878d5ec667a01dd1bf
thetravisweber <tbroweber@gmail.com>
2021-11-06 06:16:38 -0700

    updated bio

--- src/components/TravisBio.vue
+++ src/components/TravisBio.vue
@@ -3,24 +3,35 @@
 
   <div class="bio">
     <p>
-      I am a 21 year old American 🇺🇸 Software Developer currently living in
-      Scottsdale, Arizona. 
+      I am a 21 year old American 🇺🇸 Software Developer living in Flagstaff,
+      Arizona
     </p>
     <p>
-      I am currently attending Rio Salado Community College,
-      and hope to graduate from a state school with a degree in Mathematics.
+      I am currently in my third year of school at Northern Arizona University
+      pursuing a degree in Mathematics.
     </p>
     <p>
-      I have been programming since I was 16 years old. For the last 2 years, I have been working
-      almost exclusively in web development. Feel Free to check out some of 
-      <router-link to="/work">My Work</router-link>.
+      I have been programming since I was 16 years old. Feel Free to check out
+      some of
+      <router-link to="/work">My Work</router-link>. I am pursuing a degree in
+      Mathematics because I believe that this will help me out in my carreer as
+      a programmer more than a degree in Computer Science. In the 2 years of
+      work I did outside of school, I ran into problems that I could have solved
+      better if I had a better understanding high-level of Math, but few that
+      required more programming skills. This was especially true in my Machine
+      Learning Research. I am hoping that the new tools that my Mathematics
+      degree is giving me will help me better search for solutions to Machine
+      Learning problems that everyone is looking for.
     </p>
-    <p> My favotite football 🏈 team is the Miami Dolphins, and my favorite animal is also dolphins 🐬. I have 4 friends, <router-link to="/dog">a dog</router-link>, and a girlfriend. </p>
+    <p>
+      My favotite football 🏈 team is the Miami Dolphins, and my favorite animal
+      is also dolphins 🐬. I have 4 friends,
+      <router-link to="/dog">a dog</router-link>, and a girlfriend.
+    </p>
   </div>
 </template>
 
 <style scoped>
-
 .bio {
   margin: auto;
   width: 400px;
@@ -30,5 +41,4 @@
 .bio p {
   font-family: "Trebuchet MS", sans-serif;
 }
-
 </style>
