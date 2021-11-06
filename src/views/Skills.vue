af3bb40dca0f42a196be0b429241b81ecc1534e0
thetravisweber <tbroweber@gmail.com>
2021-11-06 06:10:52 -0700

    fix: spelling error, refactor: made code easier to read

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -3,15 +3,10 @@
     <h2>Skills</h2>
 
     <div class="skills-container center">
-
       <WorkTitle>Programming Skills</WorkTitle>
-      
-      <p> 
-        First and Foremost, I am a developer.
-      </p>
-      <p> 
-        I am skilled in all of the following:
-      </p>
+
+      <p>First and Foremost, I am a developer.</p>
+      <p>I am skilled in all of the following:</p>
       <ul class="list-group">
         <li class="list-group-item active">Database Architecture</li>
         <li class="list-group-item">Object Oriented Programming</li>
@@ -21,9 +16,7 @@
         <li class="list-group-item">Java</li>
         <li class="list-group-item">Dart/Flutter</li>
       </ul>
-      <p>
-        I have weaknesses in the following, mostly from lack of experience:
-      </p>
+      <p>I have weaknesses in the following, mostly from lack of experience:</p>
 
       <ul>
         <li>Front-end programming</li>
@@ -33,33 +26,28 @@
 
       <WorkTitle>Communication Skills</WorkTitle>
 
-      <p> 
-        I have spent a lot of time working with developers in other nations. As an the only American developer in the 
-        start-up that I have been working for, much of my duty has been communicating and coordinating with developers 
-        around the globe.
+      <p>
+        I have spent a lot of time working with developers in other nations. As
+        the only American developer in the start-up that I have been working
+        for, much of my duty has been communicating and coordinating with
+        developers around the globe.
       </p>
-
     </div>
-
-    
   </div>
 </template>
 
-
 <script>
-
 import WorkTitle from "@/components/WorkTitle.vue";
 
 export default {
   name: "Skills",
   components: {
-    WorkTitle
+    WorkTitle,
   },
 };
 </script>
 
 <style>
-
 .skills-container {
   width: 60%;
   min-height: 100%;
@@ -68,11 +56,11 @@ export default {
   border-color: var(--light-secondary);
 }
 
-p, li {
+p,
+li {
   text-align: left;
   text-indent: 25px;
   color: var(--black);
-  list-style-type:square;
+  list-style-type: square;
 }
-
 </style>
